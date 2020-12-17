b2981f45b411fee50b9337f65681beb8595f362f
rodneyspark <gakururodney@gmail.com>
2020-12-17 16:07:10 +0300

    added a page

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',validators=[Required()])
+    username = StringField('hotel Username',validators=[Required()])
     password = PasswordField('Password',validators=[Required()])
     remember = BooleanField('Remember Me!')
     submit = SubmitField('Login')
